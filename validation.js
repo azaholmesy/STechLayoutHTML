efecab3a95a3088355291360dc6180a793dd5dc3
Felipe Correa Soares <182100521@senacinformatica.br>
2022-06-30 17:07:14 -0300

    adding imgUser preview

--- validation.js
+++ validation.js
@@ -102,3 +102,13 @@ function mascaraTelefone(i) {
 	if (value.length == 3) i.value +=")";
 	if (value.length == 9) i.value +="-";
 }
+
+function lerImg() {
+	if(this.files && this.files[0]){
+		var file = new FileReader();
+		file.onload = function (e) {
+			document.getElementById("preview").src = e.target.result;
+		}
+		file.readAsDataURL(this.files[0]);
+	}
+}
